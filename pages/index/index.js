948b92287638101714f062ebc2b51d73c1ca8513
shadow <fshadow@yeah.net>
2017-07-19 17:33:01 +0800

    【完成】首页

---
+++ pages/index/index.js
@@ -0,0 +1,22 @@
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      '../../image/swiper/01.jpg',
+      '../../image/swiper/02.jpg',
+      '../../image/swiper/03.jpg',
+      '../../image/swiper/04.jpg',
+      '../../image/swiper/05.jpg',
+      '../../image/swiper/06.jpg',
+      '../../image/swiper/07.jpg'
+    ],
+    swiperCurrent: 0,
+  },
+  swiperChange: function (e) {
+    this.setData({
+      swiperCurrent: e.detail.current
+    })
+  }
+})
