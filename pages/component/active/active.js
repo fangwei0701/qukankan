18fdcac9544069f2a7dad8af13f7859f70d46dd9
shadow <fshadow@yeah.net>
2017-07-24 17:12:37 +0800

    【解决】列表的渲染问题；以及图片的渲染问题

--- pages/component/active/active.js
+++ pages/component/active/active.js
@@ -4,24 +4,24 @@ Page({
 
   data: {
     currentIndex: 0,
-    details: []
+    detailData: [],
+    details:[]
   },
   // 页面加载
   onLoad: function () {
+    var that = this;
     this.setData({
-      details: active_detail.active_detail_data,
+      detailData: active_detail.active_detail_data,
+      details: active_detail.active_detail_data[0].detail //默认推荐的数据
     })
   },
   // 切换导航
   switchTab: function (e) {
+    var that = this;
+    var detailData = that.data.detailData;
     this.setData({
-      currentIndex: e.target.dataset.index
+      currentIndex: e.target.dataset.id,
+      details: detailData[e.target.dataset.id].detail,
     })
-  },
-  // 触动详情页
-  swiperChange: function (e) {
-    this.setData({
-      currentIndex: e.detail.current
-    })
-  },
+  }
 })
