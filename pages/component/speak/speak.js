698ffc65a9ea7b8ba1e682e7c89b4cdeaa41adca
shadow <fshadow@yeah.net>
2017-07-28 17:19:16 +0800

    【增加】关注功能

--- pages/component/speak/speak.js
+++ pages/component/speak/speak.js
@@ -3,11 +3,13 @@ var speak_detail = require("../../../data/data_content.js");
 Page({
 
   data: {
+    concerns: '../../../../image/icon/concerns.png',
     currentIndex: 0,
     speakDetail: [],
     details: []
   },
 
+  // 页面加载
   onLoad: function () {
     this.setData({
       speakDetail: speak_detail.speak_data,
@@ -15,12 +17,19 @@ Page({
     })
   },
 
+  // 导航切换
   switchTab: function (e) {
-    var that = this;
-    var speakDetail = that.data.speakDetail;
+    let speakDetail = this.data.speakDetail;
     this.setData({
       currentIndex: e.target.dataset.id,
       details: speakDetail[e.target.dataset.id].detail,
     })
-  }
+  },
+
+  // 点击关注
+  concerns: function (e) {
+    this.setData({
+      concerns: '../../../../image/icon/concerns_active.png'
+    })
+  },
 })
