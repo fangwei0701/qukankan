64150ce32293c7789864322220002b036f3a0998
shadow <fshadow@yeah.net>
2017-08-01 17:47:06 +0800

    【增加】我的活动界面增加，导航切换，以及消息模块

--- pages/detailComponent/myActive/myActive.js
+++ pages/detailComponent/myActive/myActive.js
@@ -4,27 +4,23 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    tabNavs: ["参与的活动", "发布的活动"],
+    currentIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad(e) {
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-    
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-    
+  //  导航切换
+  switchTab(e) {
+    let index = e.currentTarget.dataset.index;
+    this.setData({
+      currentIndex: index
+    })
   }
+
 })
+
