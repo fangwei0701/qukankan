0a26dac4a1b7b5fcde54f53acbfd6b5e776cb910
shadow <fshadow@yeah.net>
2017-08-04 17:50:07 +0800

    【增加】商品详情页

---
+++ pages/detailComponent/goodsDetail/goodsDetail.js
@@ -0,0 +1,40 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    tabnavs: ["基本信息", "商品详情", "评论"],
+    imgUrls: [
+      '../../../image/goodsImg/3_01.jpg',
+      '../../../image/goodsImg/3_02.jpg',
+      '../../../image/goodsImg/3_03.jpg',
+      '../../../image/goodsImg/3_04.jpg',
+      '../../../image/goodsImg/3_05.jpg'
+    ],
+    currentIndex: 0,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  //  导航切换
+  switchTab(e) {
+    const index = e.currentTarget.dataset.index;
+    let currentIndex = this.data.currentIndex;
+    this.setData({
+      currentIndex: index
+    })
+  }
+})
