9050000fcc437fd3382ae8b95b72a00142e5989e
shadow <fangwei0701@126.com>
2017-08-05 20:26:25 +0800

    【修改】部分界面；以及增加地址编辑的跳转

--- pages/detailComponent/address/address.js
+++ pages/detailComponent/address/address.js
@@ -17,10 +17,10 @@ Page({
   
   },
 
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-  
+  //编辑地址
+  editAddress() {
+    wx.navigateTo({
+      url: './newAddress/newAddress?title=newAddress'
+    })
   }
 })
