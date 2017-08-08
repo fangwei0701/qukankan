11bf8f71e39933b1f65d894801abe61697764f56
shadow <fshadow@yeah.net>
2017-08-08 15:55:11 +0800

    【修改】活动详情界面的样式，以及类名

--- pages/detailComponent/activeDetail/activeDetail.js
+++ pages/detailComponent/activeDetail/activeDetail.js
@@ -4,13 +4,27 @@ Page({
    * 页面的初始数据
    */
   data: {
-    
+    tabnavs: [
+      { name: '活动介绍', id: 'intro' },
+      { name: '活动行程', id: 'route' },
+      { name: '费用说明', id: 'fee' },
+      { name: '更多介绍', id: 'more' }
+    ],
+    curIndex: 0
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-    
+  onLoad(e) {
+
+  },
+
+  switchTab(e) {
+    let curIndex = this.data.curIndex;
+    this.setData({
+      curIndex: e.currentTarget.dataset.index
+    })
   }
+
 })
