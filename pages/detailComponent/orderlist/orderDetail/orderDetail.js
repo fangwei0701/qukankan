c57aad638037543729fa8ea482c665a13b2bcb0b
shadow <fshadow@yeah.net>
2017-08-11 17:03:58 +0800

    【修改】订单列表采用模板，增加删除的业务逻辑；订单详情增加删除的业务，并且返回上一级

--- pages/detailComponent/orderlist/orderDetail/orderDetail.js
+++ pages/detailComponent/orderlist/orderDetail/orderDetail.js
@@ -1,7 +1,25 @@
 Page({
 
   data: {
-
+    orderDetail: {
+      'storeName': '骆驼户外官方旗舰店',
+      'goodsImg': '../../../../image/2_02.jpg',
+      'content': '【热销7.8万双】骆驼户外登山鞋男鞋秋季牛皮防滑户外越野徒步鞋',
+      'goodsNumb': '2',
+      'singlePice': '98.00',
+      'goodsTotalPice': '196.00',
+      'freight': '5.00',
+      'orderPice': '201.00',
+      'totalPay': '201.00',
+      'orderNumb': '40280089968486797',
+      'transNumb': '4004132001201707110115422290',
+      'creaTime': '2017-07-28 08:22:09',
+      'payTime': '2017-07-28 08:22:23',
+      'closTime': '2017-07-28 08:22:30',
+      'name': '万林松',
+      'phone': '18984302014',
+      'region': '贵州省贵阳市云岩区东山社区服务中心'
+    }
   },
 
   /**
@@ -15,6 +33,25 @@ Page({
   getGoodsDetail() {
     wx.redirectTo({
       url: '../../goodsDetail/goodsDetail?title=goodsDetail'
+    })
+  },
+
+  //删除订单
+  deleteOrderList(e) {
+    let that = this;
+    let orderDetail = that.data.orderDetail;
+
+    wx.showModal({
+      title: '确认删除该订单？', content: '', cancelText: '再考虑哈', confirmText: '忍心删除',
+      success: function (res) {
+        if (res.confirm) {
+          orderDetail='';
+          that.setData({
+            orderDetail: orderDetail
+          })
+          wx.navigateBack(); //删除之后返回上一界面
+        } else if (res.cancel) { }
+      }
     })
   }
 })
--- pages/detailComponent/orderlist/orderlist.js
+++ pages/detailComponent/orderlist/orderlist.js
@@ -1,13 +1,46 @@
 Page({
-
   data: {
+    orderList: [
+      {
+        'storeName': '骆驼户外官方旗舰店',
+        'goodsImg': '../../../image/2_02.jpg',
+        'content': '【热销7.8万双】骆驼户外登山鞋男鞋秋季牛皮防滑户外越野徒步鞋',
+        'goodsNumb': '2',
+        'totalPay': '285.00',
+        'freight': '5.00'
+      },
+      {
+        'storeName': '天火官方旗舰店',
+        'goodsImg': '../../../image/2_04.jpg',
+        'content': '天火LED强光手电筒 远射5000超亮变焦户外家用可充电迷你防水防身',
+        'goodsNumb': '2',
+        'totalPay': '285.00',
+        'freight': '5.00'
+      }
+    ]
+  },
+
+  onLoad: function (options) {
 
   },
 
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
+  //删除订单
+  deleteOrderList(e) {
+    const index = e.currentTarget.dataset.index;
+    let that = this;
+    let orderList = that.data.orderList;
 
+    wx.showModal({
+      title: '确认删除该订单？', content: '', cancelText: '再考虑哈', confirmText: '忍心删除',
+      success: function (res) {
+        if (res.confirm) {
+          orderList.splice(index, 1);
+          that.setData({
+            orderList: orderList
+          })
+        } else if (res.cancel) { }
+      }
+    })
   }
+
 })
